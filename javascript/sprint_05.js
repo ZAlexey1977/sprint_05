f1677bccdc31ec68d6c819d332b534d3321f6b75
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-11-29 17:52:51 +0200

    tasks 8-10

--- javascript/sprint_05.js
+++ javascript/sprint_05.js
@@ -1,22 +1,22 @@
 "use strict";
 // Task 01
 // Напишите стрелочную функцию f01 принимающую два параметра - числа и возвращающая их произведение.
-// Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно. 
+// Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 const f01 = (a, b) => {
     return a * b;
 };
-document.querySelector('.b-1').addEventListener('click', () => {
-    document.querySelector('.out-1').textContent = String(f01(3, 4));
+document.querySelector(".b-1").addEventListener("click", () => {
+    document.querySelector(".out-1").textContent = String(f01(3, 4));
 });
 // Task 02
 // Напишите стрелочную функцию f02 которая принимает строку и возвращает ее очищенную по краям (от пробелов),
 // и приведенную к нижнему регистру. Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 const f02 = (str) => {
-    return str.trim();
+    return str.trim().toLowerCase();
 };
-document.querySelector('.b-2').addEventListener('click', () => {
-    const result = f02('  Vladimir Harkonnen  ');
-    document.querySelector('.out-2').textContent = result;
+document.querySelector(".b-2").addEventListener("click", () => {
+    const result = f02("  Vladimir Harkonnen  ");
+    document.querySelector(".out-2").textContent = result;
     console.log(result);
 });
 // Task 03
@@ -24,11 +24,11 @@ document.querySelector('.b-2').addEventListener('click', () => {
 // и приведенную к нижнему регистру. Предусмотрите что аргумент может быть не обязательный - в таком случае
 // функция возвращает пустую строку. Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 const f03 = (str) => {
-    return (str) ? str.trim().toLowerCase() : '';
+    return str ? str.trim().toLowerCase() : "";
 };
-document.querySelector('.b-3').addEventListener('click', () => {
-    const result = f03(' Paul Atreides ');
-    document.querySelector('.out-3').textContent = result;
+document.querySelector(".b-3").addEventListener("click", () => {
+    const result = f03(" Paul Atreides ");
+    document.querySelector(".out-3").textContent = result;
     console.log(result);
     console.log(result.length);
 });
@@ -36,63 +36,75 @@ document.querySelector('.b-3').addEventListener('click', () => {
 // Напишите стрелочную функцию f04, которая принимает число - и возвращает true если число четное, и false в противном случае.
 // Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 const f04 = (num) => {
-    return (num % 2 == 0) ? true : false;
+    return num % 2 == 0 ? true : false;
 };
-document.querySelector('.b-4').addEventListener('click', () => {
-    document.querySelector('.out-4').textContent = String(f04(3));
+document.querySelector(".b-4").addEventListener("click", () => {
+    document.querySelector(".out-4").textContent = String(f04(3));
 });
 // Task 05
 // Напишите стрелочную функцию f05 которая принимает класс input и возвращает ЧИСЛО введенное в данный input.
 // Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 const f05 = (str) => {
     let temp = Number(document.querySelector(str).value);
-    return (temp);
+    return temp;
 };
-document.querySelector('.b-5').addEventListener('click', () => {
-    document.querySelector('.out-5').textContent = String(f05('.i-5'));
+document.querySelector(".b-5").addEventListener("click", () => {
+    document.querySelector(".out-5").textContent = String(f05(".i-5"));
 });
 // Task 06
 // Напишите стрелочную функцию - процедуру f06 которая получает строки из .i-61 и .i-62 и выводит в .out-6 большую строку.
 // Тип данных для стрелочной функции пропишите самостоятельно.
 const f06 = () => {
-    let str1 = String(document.querySelector('.i-61').value);
-    let str2 = String(document.querySelector('.i-62').value);
-    let out = '';
-    str1.length > str2.length ? out = str1 : out = str2;
-    document.querySelector('.out-6').textContent = out;
+    let str1 = String(document.querySelector(".i-61").value);
+    let str2 = String(document.querySelector(".i-62").value);
+    let out = "";
+    str1.length > str2.length ? (out = str1) : (out = str2);
+    document.querySelector(".out-6").textContent = out;
 };
-document.querySelector('.b-6').addEventListener('click', f06);
+document.querySelector(".b-6").addEventListener("click", f06);
 // Task 07
 // Напишите стрелочную функцию f07 которая получает email и проверяет что срока содержит символ '@'. Если содержит то,
 // возвращает строку, приведенную к нижнему регистру и с обрезанными по краям пробелами. Если строка не содержит @,
 // то возвращает false. Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 const f07 = (str) => {
-    return (str.includes('@')) ? str.trim().toLowerCase() : false;
+    return str.includes("@") ? str.trim().toLowerCase() : false;
 };
-document.querySelector('.b-7').addEventListener('click', () => {
-    document.querySelector('.out-7').textContent = String(f07(' glossuRaBBan@mail.ru '));
+document.querySelector(".b-7").addEventListener("click", () => {
+    document.querySelector(".out-7").textContent = String(f07(" glossuRaBBan@mail.ru "));
 });
 // Task 08
 // Напишите стрелочную функцию f08 которая получает два числа как аргументы и возвращает случайное целое число в указанном диапазоне.
 // Задайте параметрам значение по умолчанию 0 и 100. Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
-// const f08 = 
-document.querySelector('.b-8').addEventListener('click', () => {
-    //  document.querySelector('.out-8').textContent = String(f08(100, 110)); 
+const f08 = (num1 = 0, num2 = 100) => {
+    let res = Math.round(Math.random() * (num2 - num1) + num1);
+    return res;
+};
+document.querySelector(".b-8").addEventListener("click", () => {
+    document.querySelector(".out-8").textContent = String(f08(100, 110));
 });
 // Task 09
 // Напишите стрелочную функцию f09 которая возвращает строку вида 'rgb(xxx,xxx,xxx)', где xxx - случайные числа от 0 до 255
 // генерируемые функцией f08. Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
-// const f09 = 
-document.querySelector('.b-9').addEventListener('click', () => {
-    // const color : string = f09();
-    // (<HTMLElement> document.querySelector('.out-9')).style.backgroundColor = color;
-    // document.querySelector('.out-9').textContent = color;
+const f09 = () => {
+    let num = () => {
+        return Math.round(Math.random() * 255);
+    };
+    return `rgb(${num()},${num()},${num()})`;
+};
+document.querySelector(".b-9").addEventListener("click", () => {
+    const color = f09();
+    document.querySelector(".out-9").style.backgroundColor = color;
+    document.querySelector(".out-9").textContent = color;
 });
 // Task 10
 // Напишите стрелочную функцию f10 которая при каждом клике увеличивает переменную angle на 30, а потом присваивает блоку .out-10
 // свойство transform: rotate(angledeg) где angle - значение из переменной angle. Тип функций, аргументов во всех задачах
 // спринта - прописываете самостоятельно.
 let angle = 0;
-// const f10 = 
-//  document.querySelector('.b-10').addEventListener('click', f10);
+const f10 = () => {
+    angle += 30;
+    let block = document.querySelector(".out-10");
+    block.style.transform = `rotate(${angle}deg)`;
+};
+document.querySelector(".b-10").addEventListener("click", f10);
 //# sourceMappingURL=sprint_05.js.map
--- sprint_05.ts
+++ sprint_05.ts
@@ -1,28 +1,27 @@
 // Task 01
 // Напишите стрелочную функцию f01 принимающую два параметра - числа и возвращающая их произведение.
-// Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно. 
+// Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 
- const f01 = (a: number, b: number) : number => {
-    return a * b;
- }
+const f01 = (a: number, b: number): number => {
+  return a * b;
+};
 
-document.querySelector('.b-1').addEventListener('click', () : void => {
-     document.querySelector('.out-1').textContent = String(f01(3,4)); 
+document.querySelector(".b-1").addEventListener("click", (): void => {
+  document.querySelector(".out-1").textContent = String(f01(3, 4));
 });
-
 
 // Task 02
 // Напишите стрелочную функцию f02 которая принимает строку и возвращает ее очищенную по краям (от пробелов),
 // и приведенную к нижнему регистру. Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 
- const f02 = (str: string) : string => {
-    return str.trim();
- }
+const f02 = (str: string): string => {
+  return str.trim().toLowerCase();
+};
 
-document.querySelector('.b-2').addEventListener('click', () : void => {
-    const result : string = f02('  Vladimir Harkonnen  ')
-    document.querySelector('.out-2').textContent = result;
-    console.log(result);
+document.querySelector(".b-2").addEventListener("click", (): void => {
+  const result: string = f02("  Vladimir Harkonnen  ");
+  document.querySelector(".out-2").textContent = result;
+  console.log(result);
 });
 
 // Task 03
@@ -30,105 +29,116 @@ document.querySelector('.b-2').addEventListener('click', () : void => {
 // и приведенную к нижнему регистру. Предусмотрите что аргумент может быть не обязательный - в таком случае
 // функция возвращает пустую строку. Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 
-const f03 = (str?: string) : string => {
-    return (str) ? str.trim().toLowerCase() : '';
-} 
+const f03 = (str?: string): string => {
+  return str ? str.trim().toLowerCase() : "";
+};
 
-document.querySelector('.b-3').addEventListener('click', () : void => {
-    const result : string =  f03(' Paul Atreides ');
-    document.querySelector('.out-3').textContent = result;
-    console.log(result);
-    console.log(result.length); 
+document.querySelector(".b-3").addEventListener("click", (): void => {
+  const result: string = f03(" Paul Atreides ");
+  document.querySelector(".out-3").textContent = result;
+  console.log(result);
+  console.log(result.length);
 });
 
 // Task 04
 // Напишите стрелочную функцию f04, которая принимает число - и возвращает true если число четное, и false в противном случае.
 // Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 
-const f04 = (num: number) : boolean => {
-return (num % 2 == 0) ? true : false;
-}
+const f04 = (num: number): boolean => {
+  return num % 2 == 0 ? true : false;
+};
 
-document.querySelector('.b-4').addEventListener('click', () : void => {
-    document.querySelector('.out-4').textContent = String(f04(3)); 
+document.querySelector(".b-4").addEventListener("click", (): void => {
+  document.querySelector(".out-4").textContent = String(f04(3));
 });
 
 // Task 05
 // Напишите стрелочную функцию f05 которая принимает класс input и возвращает ЧИСЛО введенное в данный input.
 // Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 
-const f05 = (str:string) : number => {
-    let temp = Number((<HTMLInputElement> document.querySelector(str)).value);
-  return (temp);
-}
- 
- document.querySelector('.b-5').addEventListener('click', () : void => {
-    document.querySelector('.out-5').textContent = String(f05('.i-5')); 
-});
+const f05 = (str: string): number => {
+  let temp = Number((<HTMLInputElement>document.querySelector(str)).value);
+  return temp;
+};
 
+document.querySelector(".b-5").addEventListener("click", (): void => {
+  document.querySelector(".out-5").textContent = String(f05(".i-5"));
+});
 
 // Task 06
 // Напишите стрелочную функцию - процедуру f06 которая получает строки из .i-61 и .i-62 и выводит в .out-6 большую строку.
 // Тип данных для стрелочной функции пропишите самостоятельно.
 
-const f06 = () : void => {
+const f06 = (): void => {
+  let str1: string = String(
+    (<HTMLInputElement>document.querySelector(".i-61")).value
+  );
+  let str2: string = String(
+    (<HTMLInputElement>document.querySelector(".i-62")).value
+  );
+  let out = "";
+  str1.length > str2.length ? (out = str1) : (out = str2);
+  document.querySelector(".out-6").textContent = out;
+};
 
-  let str1 : string = String((<HTMLInputElement> document.querySelector('.i-61')).value);
-  let str2 : string = String((<HTMLInputElement> document.querySelector('.i-62')).value);
-  let out = '';
-  str1.length > str2.length ? out = str1 : out = str2;
-  document.querySelector('.out-6').textContent = out;
-}
-
-document.querySelector('.b-6').addEventListener('click', f06);
-
+document.querySelector(".b-6").addEventListener("click", f06);
 
 // Task 07
 // Напишите стрелочную функцию f07 которая получает email и проверяет что срока содержит символ '@'. Если содержит то,
 // возвращает строку, приведенную к нижнему регистру и с обрезанными по краям пробелами. Если строка не содержит @,
 // то возвращает false. Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 
-const f07 = (str:string) : string | boolean => {
+const f07 = (str: string): string | boolean => {
+  return str.includes("@") ? str.trim().toLowerCase() : false;
+};
 
-  return (str.includes('@')) ? str.trim().toLowerCase() : false;
+document.querySelector(".b-7").addEventListener("click", (): void => {
+  document.querySelector(".out-7").textContent = String(
+    f07(" glossuRaBBan@mail.ru ")
+  );
+});
 
-}
- 
- document.querySelector('.b-7').addEventListener('click', () : void => {
-    document.querySelector('.out-7').textContent = String(f07(' glossuRaBBan@mail.ru ')); 
- });
-
-
- // Task 08
+// Task 08
 // Напишите стрелочную функцию f08 которая получает два числа как аргументы и возвращает случайное целое число в указанном диапазоне.
 // Задайте параметрам значение по умолчанию 0 и 100. Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 
-// const f08 = 
- 
- document.querySelector('.b-8').addEventListener('click', () : void => {
-    //  document.querySelector('.out-8').textContent = String(f08(100, 110)); 
- });
+const f08 = (num1: number = 0, num2: number = 100): number => {
+  let res = Math.round(Math.random() * (num2 - num1) + num1);
+  return res;
+};
 
- // Task 09
+document.querySelector(".b-8").addEventListener("click", (): void => {
+  document.querySelector(".out-8").textContent = String(f08(100, 110));
+});
+
+// Task 09
 // Напишите стрелочную функцию f09 которая возвращает строку вида 'rgb(xxx,xxx,xxx)', где xxx - случайные числа от 0 до 255
 // генерируемые функцией f08. Тип функций, аргументов во всех задачах спринта - прописываете самостоятельно.
 
-// const f09 = 
- 
- document.querySelector('.b-9').addEventListener('click', () : void => {
-    // const color : string = f09();
-    // (<HTMLElement> document.querySelector('.out-9')).style.backgroundColor = color;
-    // document.querySelector('.out-9').textContent = color;
- });
+const f09 = (): string => {
+  let num = () => {
+    return Math.round(Math.random() * 255);
+  };
+  return `rgb(${num()},${num()},${num()})`;
+};
 
- // Task 10
+document.querySelector(".b-9").addEventListener("click", (): void => {
+  const color: string = f09();
+  (<HTMLElement>document.querySelector(".out-9")).style.backgroundColor = color;
+  document.querySelector(".out-9").textContent = color;
+});
+
+// Task 10
 // Напишите стрелочную функцию f10 которая при каждом клике увеличивает переменную angle на 30, а потом присваивает блоку .out-10
 // свойство transform: rotate(angledeg) где angle - значение из переменной angle. Тип функций, аргументов во всех задачах
 // спринта - прописываете самостоятельно.
 
-let angle = 0;
+let angle: number = 0;
 
-// const f10 = 
- 
-//  document.querySelector('.b-10').addEventListener('click', f10);
+const f10 = (): void => {
+  angle += 30;
+  let block = <HTMLDivElement>document.querySelector(".out-10");
+  block.style.transform = `rotate(${angle}deg)`;
+};
 
+document.querySelector(".b-10").addEventListener("click", f10);
